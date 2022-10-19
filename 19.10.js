cc9010d8fb915b1d43da89742db692c4cc3f091d
Tatiana <tatyanakoroleva00@mail.ru>
2022-10-19 13:20:41 +0200

    exercices done

---
+++ 19.10.js
@@ -0,0 +1,14 @@
+            //1. Const and let in objects
+/*
+const arr = [1, 2, 3, 4, 5];
+const res = arr[1] + arr[2];
+console.log(res);
+*/
+
+            //2. Mistakes correction
+/*
+let obj = {x: 1, y: 2, z: 3};
+console.log(obj['x']);
+*/
+
+            //3.
