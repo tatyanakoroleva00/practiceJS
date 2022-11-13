42f9ea7a89a1b4411fb5e40102c581070ed52193
Tatiana <tatyanakoroleva00@mail.ru>
2022-11-13 12:43:40 +0200

    exercices done

---
+++ JS_Dronov_book_practice/Lesson_3/3.8.js
@@ -0,0 +1,13 @@
+function circleLength (d) {
+    let l;
+    l = d * Math.PI;
+    return l;
+}
+
+let x = parseInt('5');
+document.write(`${x} <p>`);
+
+let y = parseFloat('5.23');
+document.write(`${y} </p>`);
+
+
