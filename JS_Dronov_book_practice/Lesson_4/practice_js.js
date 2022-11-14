d1cfc92defe1cdfc1431841a5b25966eebc510be
Tatiana <tatyanakoroleva00@mail.ru>
2022-11-14 15:16:38 +0200

    exercices done

---
+++ JS_Dronov_book_practice/Lesson_4/practice_js.js
@@ -0,0 +1,25 @@
+/*          1. Method Number.isInteger()
+let a = 14;
+console.log(Number.isInteger(a)); */
+
+/*          2. Method Number.toString();
+let a = 15;
+let str = a.toString();
+console.log(typeof(str)); */
+
+/*         3. Method .toFixed()
+let a = 14.678;
+let result = a.toFixed();
+console.log(result); */
+
+/*         4. .toUppercase()
+let a = 'helloKate';
+let str = a.toUpperCase();
+console.log(str); */
+
+ /*        5. .toLowercase()
+let a = 'HELLO, HOW ARE YOU?';
+console.log(a.toLowerCase()); */
+
+
+
