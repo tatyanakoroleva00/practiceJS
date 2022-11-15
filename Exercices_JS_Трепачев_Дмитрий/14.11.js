3251849b319b2ce4157e745edd7a3b3204db850e
Tatiana <tatyanakoroleva00@mail.ru>
2022-11-15 11:41:50 +0200

    exercices done

---
+++ Exercices_JS_Трепачев_Дмитрий/14.11.js
@@ -0,0 +1,79 @@
+/*     1. 
+let a = Math.pow(2, 10);
+console.log(a);
+ */
+   /*  2. 
+let a = Math.sqrt(245);
+console.log(a); */
+
+ /*   3. Map method or cycle for 
+let arr = [4, 2, 5, 19, 13, 0, 10];
+/* let arr1 = arr.map((el) => Math.pow(el, 3));
+console.log(arr1); */
+/* let sum = 0;
+for (let i = 0; i < arr.length; i++) {
+    arr[i] = Math.pow(arr[i], 3);
+    sum += arr[i];
+}
+console.log(Math.sqrt(sum)); */
+
+ /*    4. Rounding
+let num = Math.sqrt(379);
+console.log(num);
+num = Math.floor(num);
+console.log(num);
+num = num.toFixed(1);
+console.log(num);
+num = num.toFixed(2);
+console.log(num); */
+
+/*     5. 
+let num = 587;
+num = Math.sqrt(num);
+console.log(num);
+/* num = Math.floor(num);
+console.log(num); */
+/* num = Math.ceil(num);
+console.log(num); */ 
+
+  /*   6. Min & Max
+let arr = [4, -2, 5, 19, -130, 0, 10];
+let max = Math.max.apply(null, arr);
+console.log(max);
+let min = Math.min.apply(null, arr);
+console.log(min); */
+
+/*     7.
+function getRandomInt(min, max) {
+    result = Math.floor(Math.random() * (max - min + 1)) + min;
+    return result;
+}
+console.log(getRandomInt(1, 100));
+ */
+
+/*     8. Fill the array with random numbers
+let arr = [];
+for (let i = 0; i < 10; i++) {
+    let num = Math.random();
+    arr.push(num);
+}
+console.log(arr);
+ */
+
+ /*    9. 
+let a = 15;
+let b = -5;
+let result = Math.abs(a - b);
+console.log(result); */
+
+/*     10.
+let str = 'js';
+str = str.toUpperCase();
+console.log(str); */
+
+/*     11. 
+let str = 'JS';
+str = str.toLowerCase();
+console.log(str);
+ */
+
