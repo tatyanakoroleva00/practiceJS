8527a8c29fe0925754946199c79b798eae40943c
Tatiana <tatyanakoroleva00@mail.ru>
2022-12-02 22:29:32 +0200

    exercices done

--- Codewars/8.3K.js
+++ Codewars/8.3K.js
@@ -7,3 +7,107 @@ function binToDec(bin){
 function noBoringZeros(n) {
     return Number(n.toString().replace(/0+$/, ''));
     } */
+
+   /*  153. Training JS #2: Basic data types--Number
+    let v1 = 50,
+    v2 = 100,
+    v3 = 150,
+    v4 = 200,
+    v5 = 2,
+    v6 = 250;
+
+function equal1(){
+  let a = v1,   
+      b = v1;   
+  return a + b;
+}
+
+//Please refer to the example above to complete the following functions
+function equal2(){
+  let a = v3, //set number value to a
+      b = v1; //set number value to b
+  return a - b;
+}
+
+function equal3(){
+  let a = v1, //set number value to a
+      b = v5; //set number value to b
+  return a * b;
+}
+
+function equal4(){
+  let a = v4, //set number value to a
+      b = v5; //set number value to b
+  return a / b;
+}
+
+function equal5(){
+  let a = v6, //set number value to a
+      b = v3; //set number value to b
+  return a % b;
+} */
+
+/*     154. Plural
+function plural(n) {
+    return n === 1 ? false : true
+    } */
+
+/*     155. Enumerable Magic #25 - Take the First N Elements
+function take(arr, n) {
+    return arr.slice(0, n)
+    } */
+
+/*     156. String Templates - Bug Fixing #5
+function buildString(...template){
+    return `I like ${template.join(', ')}!`;
+    } */
+    
+
+/*     157. Find the position!
+function position(letter){
+    let str = 'abcdefghijklmnopqrstuvwxyz';
+    let arr = str.split('');
+    return 'Position of alphabet: ' + (arr.indexOf(letter) + 1);
+    } */
+
+/*     158. Holiday VIII - Duty Free
+function dutyFree(normPrice, discount, hol){
+    return Math.floor(hol / (normPrice / 100 * discount));
+    } */
+
+   /* 159. Training JS #4: Basic data types--Array
+function getLength(arr){
+//return length of arr
+return arr.length;
+}
+function getFirst(arr){
+//return the first element of arr
+return arr[0];
+}
+function getLast(arr){
+//return the last element of arr
+return arr[arr.length - 1];
+}
+
+function pushElement(arr){
+var el=1;
+//push el to arr
+arr.push(el)
+return arr
+}
+
+function popElement(arr){
+//pop an element from arr
+    arr.pop()
+    return arr
+} */
+
+ /*    160. String cleaning
+function stringClean(s){
+    return s.replace(/[0-9]/g,'');
+    } */
+
+/*     161. Convert to Binary
+function toBinary(n){
+    return Number(n.toString(2))
+    } */
