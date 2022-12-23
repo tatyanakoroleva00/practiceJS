aa449df936b199b706e4ad86dbd3260633468b62
Tatiana <tatyanakoroleva00@mail.ru>
2022-12-23 11:00:08 +0300

    twelve exercices are done

---
+++ Exercices_JS_Трепачев_Дмитрий/December_2022/22.12.js
@@ -0,0 +1,128 @@
+/* Functions */
+
+/* 1.
+function func() {
+    console.log('!');
+}
+func();
+func();
+func(); */
+
+/* func();
+function func() {
+    console.log('hello');
+} */
+
+/*     1. Output your surname
+function func(name) {
+    console.log(name);
+}
+func('Tatiana');  */
+
+/* function func() {
+    console.log('Tatiana');
+} 
+func(); */
+/*     2. Find the sum of numbers from 1 to 100
+func ();
+function func() {
+    let sum = 0;
+    for (let i = 1; i <= 100; i++) {
+        sum += i;
+    }
+    console.log(sum);
+} */
+/*     3. Raise the number to a cube
+    func(3);
+    function func(num) {
+        console.log(Math.pow(num, 3));
+    } */
+
+   /*  4. 
+    func(10);
+    function func (num) {
+        (num > 0) ? console.log('+++') : console.log('---');
+    } */
+
+    /*     5.
+    function sum(a, b, c) {
+        console.log(a + b + c);
+    }
+    sum(1, 5, 8); */
+
+  /*   6. Sum three parameters
+    let param1 = 1;
+    let param2 = 2;
+    let param3 = 3;
+
+    func(param1, param2, param3);
+
+    function func(a, b, c) {
+        console.log(a + b + c);
+    } */
+
+     /*    7. Raise the number to a cube
+    function func(num) {
+        return (num ** 3);
+    }
+    let res = func(3);
+    console.log(res); */
+
+     /*    8. Sum square roots of two numbers
+    function func(num) {
+        return Math.sqrt(num, 2);
+    }
+    let sum = func(3) + func(4);
+    console.log(sum); */
+
+       /*  9.
+    function sqrt(num) {
+        return Math.sqrt(num);
+    }
+    
+    function round(num) {
+        return num.toFixed(3);
+    }
+    let res = round(sqrt(2));
+    console.log(res); */
+
+      /*   10. Return the sum of square roots of three numbers
+    function sqrt(num) {
+        return Math.sqrt(num);
+    }
+    
+    function sum(num1, num2, num3) {
+        return num1 + num2 + num3;
+    }
+
+    let res = sum(sqrt(2), sqrt(3), sqrt(4));
+    console.log(res); */
+
+        /* 11. 
+    function sqrt(num) {
+        return Math.sqrt(num);
+    }
+    
+    function sum(num1, num2, num3) {
+        return num1 + num2 + num3;
+    }
+
+    let res = sum(sqrt(2), sqrt(3), sqrt(4));
+    
+    function round(a) {
+        return a.toFixed(3);
+    }
+    
+    let res2 = round(res);
+    console.log(res2);
+ */
+    /* 12. 
+    function number(a) {
+        let i = 0;
+        while (a >= 10) {
+            a = a - 2;
+            i++;
+        }
+        console.log(i - 1);
+    }
+    number(20); */
