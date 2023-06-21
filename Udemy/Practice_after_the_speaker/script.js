77eec5444dcd6911fa81ed71c587b1b9cdf9f6a6
Tatiana <tatyanakoroleva00@mail.ru>
2023-06-21 20:04:35 +0300

    practice with UDEMY course - modal window, date, setTimeOut...

--- Udemy/Practice_after_the_speaker/script.js
+++ Udemy/Practice_after_the_speaker/script.js
@@ -25,58 +25,117 @@
 //     }})
         
 
-/* CREATED BY MYSELF, SLIDER */
+// /* CREATED BY MYSELF, SLIDER */
 
-function sliderToCreate() {
+// function sliderToCreate() {
 
 
-const images = document.querySelector('.image-box');
-const image = document.querySelectorAll('.img');
-const rightArrow = document.querySelector('.right_arrow');
-const leftArrow = document.querySelector('.left_arrow');
+// const images = document.querySelector('.image-box');
+// const image = document.querySelectorAll('.img');
+// const rightArrow = document.querySelector('.right_arrow');
+// const leftArrow = document.querySelector('.left_arrow');
 
-const i = 0;
-let n = image.length;
+// const i = 0;
+// let n = image.length;
 
-currentSlide();
+// currentSlide();
 
-function currentSlide(i = 0) {
-    image.forEach((item, index) => {
-        if (index === i) {
-            item.style.display = 'block';
-            item.classList.add('currentSlide');
-        } else {
-            item.style.display = 'none';
-            item.classList.add('notCurrentSlide');
-        }
+// function currentSlide(i = 0) {
+//     image.forEach((item, index) => {
+//         if (index === i) {
+//             item.style.display = 'block';
+//             item.classList.add('currentSlide');
+//         } else {
+//             item.style.display = 'none';
+//             item.classList.add('notCurrentSlide');
+//         }
 
-    })
-}
+//     })
+// }
 
-function nextSlide(i) {
-        rightArrow.addEventListener('click', () => {
-            if (i >= n) {
-                i = 0; 
-            }
-        currentSlide(i);
-        i++;
-})
-}
-nextSlide(1);
+// function nextSlide(i) {
+//         rightArrow.addEventListener('click', () => {
+//             if (i >= n) {
+//                 i = 0; 
+//             }
+//         currentSlide(i);
+//         i++;
+// })
+// }
+// nextSlide(1);
 
-function previousSlide(i) {
-    leftArrow.addEventListener('click', () => {
-        if (i < 0) {
-            i = n - 1;
-        }
-        currentSlide(i);
-        i--;
+// function previousSlide(i) {
+//     leftArrow.addEventListener('click', () => {
+//         if (i < 0) {
+//             i = n - 1;
+//         }
+//         currentSlide(i);
+//         i--;
 
-})
-}
-previousSlide(-1);
+// })
+// }
+// previousSlide(-1);
 
     
-}
+// }
 
-sliderToCreate();
+// sliderToCreate();
+
+// function timer() {
+//     const timerID = setTimeout(function() {
+//     console.log('hello');
+// }, 1000);
+// }
+// timer();
+
+// const timerID = setTimeout(logger, 1000);
+// function logger() {
+//     console.log('Hello!');
+// }
+// setInterval(logger, 1000);
+
+/*******************************DATE ******************/
+// const now = new Date();
+// console.log(now);
+
+// const now = new Date('2022-01-03');
+// console.log(now);
+
+
+// function updateTime() {
+//     const now = new Date();
+//     console.log(now);
+//     const setTime = now.setHours(10, 41);
+//     console.log(setTime);
+//     const hours = (setTime / 1000 / 60 / 60);
+//     console.log(Math.floor(hours));
+// }
+// updateTime();
+
+
+/********************MODAL WINDOW ***********/
+
+function showModalWindow() {
+    const btn = document.querySelector('.button-close');
+    const modal = document.querySelector('.data-modal');
+    const timer = setTimeout(openModal, 3000); /* Появляется через 3 секунды */
+
+    function openModal() {
+        modal.classList.add('show');
+        document.body.style.overflow = 'hidden';
+    }
+     
+    btn.addEventListener('click', () => {
+        modal.classList.remove('show');
+        modal.classList.add('hide');
+    } )
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code = 'Escape') {
+            modal.classList.remove('show');
+            modal.classList.add('hide');
+        }
+    })
+}
+showModalWindow();
+
